666dcfe90b2f36048623252a0381074389129ef2
Haider <haiderlek@gmail.com>
2021-08-25 04:31:30 +0300

    Disable the Nominate button after click the button is disabled when we add the movie to nominates and is activated when we remove the movie from nominates

--- src/components/movies/Movie.jsx
+++ src/components/movies/Movie.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import moviesContext from '../../store/moviesContext';
 function Movie({ movie, onNominate }) {
+  const { moviesState } = useContext(moviesContext);
+  const { nominates } = moviesState;
+  const checkNominated = nominates.filter((mov) => {
+    return mov.imdbID === movie.imdbID;
+  });
+
   return (
     <div>
       <h3> {movie.Title} </h3>
       <h5> {movie.Year} </h5>
       <img src={movie.Poster} alt="" />
-      <button onClick={() => onNominate(movie.imdbID)}>Nominate</button>
+      {checkNominated.length < 1 ? (
+        <button onClick={() => onNominate(movie.imdbID)}>Nominate</button>
+      ) : (
+        <button disabled>Nominated</button>
+      )}
     </div>
   );
 }
